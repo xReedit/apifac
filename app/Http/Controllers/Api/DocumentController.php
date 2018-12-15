d34eb7dfb0f7ec373e9fc921c37a7cb7c04e607d
PERU CPE <perucpe@gmail.com>
2018-12-15 13:39:43 -0500

    json de facturas

--- app/Http/Controllers/Api/DocumentController.php
+++ app/Http/Controllers/Api/DocumentController.php
@@ -60,11 +60,6 @@ class DocumentController extends Controller
 
                 $util->createPdf($document->id);
 
-                $actions = $request->input('actions');
-                $send_email = false;
-                if($actions['send_email']) {
-                    $send_email = $this->email($document->id);
-                }
                 DB::commit();
                 return [
                     'success' => true,
@@ -79,8 +74,7 @@ class DocumentController extends Controller
                         'link_xml' => $document->download_external_xml,
                         'link_pdf' => $document->download_external_pdf,
                         'link_cdr' => $document->download_external_cdr,
-                    ],
-                    'send_email' => $send_email,
+                    ]
                 ];
             }
         } catch (Exception $e) {
@@ -91,46 +85,5 @@ class DocumentController extends Controller
                 'code' => "File: {$e->getFile()}, Line: {$e->getLine()}"
             ];
         }
-
-//
-//        $document = DB::transaction(function () use($request, $document_type_code) {
-//            switch ($document_type_code) {
-//                case '01':
-//                case '03':
-//                    $builder = new InvoiceBuilder();
-//                    break;
-//                case '07':
-//                    $builder = new NoteCreditBuilder();
-//                    break;
-//                case '08':
-//                    $builder = new NoteDebitBuilder();
-//                    break;
-////                default:
-////                    throw new Exception('Tipo de documento ingresado es inválido');
-//            }
-//
-//            $builder->save($request->all());
-//            $xmlBuilder = new XmlBuilder();
-//            $xmlBuilder->createXMLSigned($builder);
-//            $document = $builder->getDocument();
-//
-//            return $document;
-//        });
-//
-//        return [
-//            'success' => true,
-//            'data' => [
-//                'id' => $document->id,
-//                'number' => $document->number_full,
-//                'hash' => $document->hash,
-//                'qr' => $document->qr,
-//                'filename' => $document->filename,
-//                'external_id' => $document->external_id,
-//                'number_to_letter' => $document->number_to_letter,
-//                'link_xml' => $document->download_external_xml,
-//                'link_pdf' => $document->download_external_pdf,
-//                'link_cdr' => $document->download_external_cdr,
-//            ]
-//        ];
     }
 }
