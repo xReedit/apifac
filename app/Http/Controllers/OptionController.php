a7498f779b877830b7a0927ad4bee334c54e06ec
PERU CPE <perucpe@gmail.com>
2018-12-22 15:34:06 -0500

    eliminacion de documentos demo

--- app/Http/Controllers/OptionController.php
+++ app/Http/Controllers/OptionController.php
@@ -17,6 +17,8 @@ class OptionController extends Controller
     {
         Summary::where('soap_type_id', '01')->delete();
         Voided::where('soap_type_id', '01')->delete();
+        Document::where('soap_type_id', '01')
+                ->whereIn('document_type_id', ['07', '08'])->delete();
         Document::where('soap_type_id', '01')->delete();
 
         return [
